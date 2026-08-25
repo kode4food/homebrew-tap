ac78bd91425b12345773db31820ddab1a7c2daf9
Thomas Bradford <thom@kode4food.it>
2026-08-25 08:02:05 +0200

    v0.3.3 release

--- Formula/toe@next.rb
+++ Formula/toe@next.rb
@@ -1,8 +1,8 @@
 class ToeATNext < Formula
   desc "Modal terminal editor for Go development (unstable channel)"
   homepage "https://github.com/kode4food/toe"
-  url "https://github.com/kode4food/toe/archive/refs/tags/v0.3.2.tar.gz"
-  sha256 "3bb118454dba36cde987d51f7b87d9b4a218b2860af25ff61d6a9b72fc492a91"
+  url "https://github.com/kode4food/toe/archive/refs/tags/v0.3.3.tar.gz"
+  sha256 "c6d621cd7119fc1d75b3b25b6603043bf1adadd54f02416caceada1908ebe558"
   license "MIT"
 
   keg_only :versioned_formula
