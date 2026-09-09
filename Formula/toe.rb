836fbd02b97260109736f8c4e86b1a154be207ee
Thomas Bradford <thom@kode4food.it>
2026-09-09 12:47:36 +0200

    v0.4.2 release

--- Formula/toe.rb
+++ Formula/toe.rb
@@ -1,8 +1,8 @@
 class Toe < Formula
   desc "Modal terminal editor for Go development"
   homepage "https://github.com/kode4food/toe"
-  url "https://github.com/kode4food/toe/archive/refs/tags/v0.4.1.tar.gz"
-  sha256 "b61ec4320b5f5694d006b01248935afc96e8c143f03b8e9f10bfb81dcfdd8b38"
+  url "https://github.com/kode4food/toe/archive/refs/tags/v0.4.2.tar.gz"
+  sha256 "2ae2e17a55b0d7b09b5feb43e4dc590fdd466b8ba3ee18c0d23e1bc12bedacc0"
   license "MIT"
 
   depends_on "go" => :build
